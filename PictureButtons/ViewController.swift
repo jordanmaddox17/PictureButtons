f2f298600b72aeb91372415411f4c553a07f486e
Jordan <jordanjean17@gmail.com>
2020-01-23 09:44:02 -0800

    SuitsInformationApp

--- PictureButtons/ViewController.swift
+++ PictureButtons/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spade: UILabel!
+    @IBOutlet weak var heart: UILabel!
+    @IBOutlet weak var clover: UILabel!
+    @IBOutlet weak var diamond: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func spadeButton(_ sender: UIButton) {
+        if spade.text != "SPADES!" {
+            spade.text = "SPADES!"
+        } else {
+            spade.text = "black suit"
+        }
+    }
+    
+    @IBAction func heartButton(_ sender: UIButton) {
+        if heart.text != "HEARTS!" {
+            heart.text = "HEARTS!"
+        } else {
+            heart.text = "red suit"
+        }
+    }
+    
+    @IBAction func cloverButton(_ sender: UIButton) {
+        if clover.text != "CLOVERS!" {
+            clover.text = "CLOVERS!"
+        } else {
+            clover.text = "black suit"
+        }
+    }
+    
+    @IBAction func diamondButton(_ sender: UIButton) {
+        if diamond.text != "DIAMONDS!" {
+            diamond.text = "DIAMONDS!"
+        } else {
+            diamond.text = "red suit"
+        }
+    }
+    
 }
 
